fb724574d039e334f8c25717b5365394b75d4707
Chuks Ossai <ossaiwisdom48@gmail.com>
2020-08-31 23:07:51 +0100

    Setup socketio server to listen to events from client

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, 'public')))
 const PORT = process.env.PORT || 3002;
+const publicDir = path.join(__dirname, 'public');
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.use(express.static(publicDir))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicDir, 'index.html'));
+});
+
+io.on('connection', socket => {
+    socket.emit('connected', 'User has connected to chat');
+
+    socket.broadcast.emit('joined', 'User has joined the chat');
+
+    socket.on('disconnect', () => {
+        io.emit('left', 'User has left the chat');
+    })
+    
+});
+
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
